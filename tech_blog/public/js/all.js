0418d145cd50f0f43e22f1da34aa0163f3246d0d
ChristopherGoodale <54218591+ChristopherGoodale@users.noreply.github.com>
2023-04-16 14:59:42 -0500

    Delete tech_blog directory

--- tech_blog/public/js/all.js
+++
@@ -1,8 +0,0 @@
-const viewComments = async (event) => {
-  if (event.target.hasAttribute("data-id")) {
-    const id = event.target.getAttribute("data-id");
-    document.location.replace(`/comment/${id}`);
-  }
-};
-
-document.querySelector("#mainBody").addEventListener("click", viewComments);
--- tech_blog/public/js/comment.js
+++
@@ -1,7 +0,0 @@
-const id = document.querySelector("#postId").getAttribute("data-id");
-async function createComment(event) {
-  event.preventDefault();
-  document.location.replace(`/addcomment/${id}`);
-}
-
-document.querySelector("#createCommentBtn").addEventListener("click", createComment);
